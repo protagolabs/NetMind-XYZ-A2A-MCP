6d6b6d4381a7b361adbee858d8084f23512d66b1
askfiy <c2323182108@gmail.com>
2025-05-21 19:00:56 +0800

    chore(streaming): Support streaming.

--- core/a2a/server.py
+++ core/a2a/server.py
@@ -188,20 +188,18 @@ class BaseXyzA2AServer(A2AServer):
 
         @app.route("/<int:agent_id>/agent.json", methods=["GET"])
         def agent_card(agent_id: int):
-            card: AgentCard = self.get_agent_card(agent_id)
-            print(f"Card 信息: {card}")
-            return jsonify(card.to_dict())
+            return jsonify({"capabilities": {"streaming": True}})
 
         @app.route("/<int:agent_id>/a2a/agent.json", methods=["GET"])
         def enhanced_a2a_agent_json(agent_id: int):
-            card: AgentCard = self.get_agent_card(agent_id)
-            print(f"Card 信息: {card}")
-            return jsonify(card.to_dict())
+            return jsonify({"capabilities": {"streaming": True}})
 
         @app.route("/.well-known.json", methods=["GET"])
         def get_agent_card():
             agent_id = request.args.get("agent_id", type=int)
-            return agent_card(agent_id)
+            card: AgentCard = self.get_agent_card(agent_id)
+            print(f"Card 信息: {card}")
+            return jsonify(card.to_dict())
 
         @app.route("/<int:agent_id>/a2a/tasks/send", methods=["POST"])
         def a2a_tasks_send(agent_id: int):
