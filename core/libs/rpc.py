5cb52fc73629a50e898f916be36b5a73c3738ffe
askfiy <c2323182108@gmail.com>
2025-05-23 11:05:15 +0800

    chore(rpc): Use RpcExecutor submit task

--- core/libs/rpc.py
+++ core/libs/rpc.py
@@ -1,3 +1,10 @@
+import atexit
+import logging
+import asyncio
+import threading
+from concurrent.futures import Future
+from typing import Coroutine, Any
+
 from multi_agent_centre.client.agent import AgentClient
 
 from core.env_helper import EnvHelper
@@ -22,3 +29,52 @@ class RpcManager:
             host=EnvHelper.get_multi_agent_host(),
             port=EnvHelper.get_multi_agent_port(),
         )
+
+
+class RpcExecutor:
+    def __init__(self):
+        self._loop = None
+        self._thread = None
+        self._running = False
+
+        self.start()
+
+    def start(self):
+        if self._running:
+            return
+
+        atexit.register(self.stop)
+        self._loop = asyncio.new_event_loop()
+        loop_started_event = threading.Event()
+        self._thread = threading.Thread(
+            target=self._run_loop, args=(loop_started_event,), daemon=True
+        )
+        self._running = True
+        self._thread.start()
+        loop_started_event.wait()
+        logging.info("gRPC 执行器已启动")
+
+    def _run_loop(self, started_event: threading.Event):
+        asyncio.set_event_loop(self._loop)
+        started_event.set()
+
+        try:
+            self._loop.run_forever()
+        finally:
+            if hasattr(self._loop, "shutdown_asyncgens"):  # Python 3.6+
+                self._loop.run_until_complete(self._loop.shutdown_asyncgens())
+            self._loop.close()
+
+    def stop(self):
+        if not self._running or not self._loop or not self._loop.is_running():
+            return
+
+        self._loop.call_soon_threadsafe(self._loop.stop)
+        self._thread.join(timeout=5)
+        self._running = False
+        logging.info("gRPC 执行器已停止")
+
+    def submit_coroutine(self, coro: Coroutine[Any, Any, Any]) -> Future:
+        if not self._running or not self._loop:
+            raise RuntimeError("GrpcAioExecutor is not running.")
+        return asyncio.run_coroutine_threadsafe(coro, self._loop)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-import asyncio
+import queue
 
 from google.protobuf.json_format import MessageToDict
 
@@ -9,8 +9,10 @@ from core.a2a.server import (
     Message,
     AsyncGenerator,
 )
-from core.libs.rpc import RpcManager
+from core.libs.rpc import RpcManager, RpcExecutor
 from core.models import MakeResponseModel
+
+rpc_executor = RpcExecutor()
 
 
 class XyzA2AServer(BaseXyzA2AServer):
@@ -26,28 +28,55 @@ class XyzA2AServer(BaseXyzA2AServer):
     async def xyz_stream_response(
         self, agent_id: int, message: Message
     ) -> AsyncGenerator[str, None]:
-        model = MakeResponseModel.model_validate_json(json_data=message.content.text)
-        logging.info(f"收到来自 {model.user_id} 发送给 {agent_id} 的信息 {message}")
+        """
+        使用 gRPC 执行器来通过 gRPC/IO 来发送任务. 避免多线程竞争问题.
+        """
+        q = queue.Queue()
 
-        async with await RpcManager.get_agent_client(agent_id=agent_id) as agent:
+        async def _xyz_stream_response():
+            model = MakeResponseModel.model_validate_json(
+                json_data=message.content.text
+            )
+            logging.info(f"收到来自 {model.user_id} 发送给 {agent_id} 的信息 {message}")
+            async with await RpcManager.get_agent_client(agent_id=agent_id) as agent:
+                try:
+                    async for msg in agent.run_message_streaming(
+                        messages_list=model.messages_list,
+                        user_id=str(model.user_id),
+                        mcp_info_list=model.mcp_info_list,
+                        other_data=model.other_data,
+                    ):
+                        msg = MessageToDict(msg)
+
+                        if msg["type"] == "stream_content":
+                            content = msg["data"]["content"]
+                            q.put(content)
+                        else:
+                            q.put(None)
+                except Exception as exc:
+                    q.put(None)
+                    logging.error(f"生成回复时错误: {exc}")
+                    raise exc
+
+        future = rpc_executor.submit_coroutine(_xyz_stream_response())
+
+        while True:
             try:
-                async for msg in agent.run_message_streaming(
-                    messages_list=model.messages_list,
-                    user_id=str(model.user_id),
-                    mcp_info_list=model.mcp_info_list,
-                    other_data=model.other_data,
-                ):
-                    msg = MessageToDict(msg)
+                item = q.get()
 
-                    if msg["type"] == "stream_content":
-                        content = msg["data"]["content"]
-                        yield content
-                    else:
+                if item is not None:
+                    yield item
+                else:
+                    break
+
+            except queue.Empty:
+                if future.done():
+                    exc = future.exception()
+                    if exc:
+                        logging.error(f"运行 rpc 执行器 err: {exc}")
                         break
-
-            except Exception as exc:
-                logging.error(f"生成回复时错误: {exc}")
-                raise exc
+                else:
+                    pass
 
     async def xyz_handle_message(self, agent_id: int, message: Message) -> Message:
         pass
