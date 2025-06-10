4845c4f0995d95f73476642248b658c5aee95710
askfiy <c2323182108@gmail.com>
2025-06-10 18:10:22 +0800

    chore(log): Add log

--- main.py
+++ main.py
@@ -92,11 +92,13 @@ class XyzA2AServer(BaseXyzA2AServer):
                     ):
                         msg = MessageToDict(msg)
 
+                        logging.info("*" * 100)
+                        logging.info(msg)
+                        logging.info("*" * 100)
+
                         if msg["type"] == "stream_content":
                             content = msg["data"]["content"]
                             q.put(content)
-                        else:
-                            logging.info(msg)
 
                 except Exception as exc:
                     q.put(traceback.format_exc())
